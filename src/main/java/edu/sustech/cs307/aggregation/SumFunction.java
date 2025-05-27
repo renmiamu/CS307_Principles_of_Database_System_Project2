2810a26d2d8f19a19d52249f2a2a47e81d1335f6
XimingZheng <12311011@mail.sustech.edu.cn>
2025-05-27 17:53:14 +0800

    AggregateFunctions(sum)

--- src/main/java/edu/sustech/cs307/aggregation/SumFunction.java
+++ src/main/java/edu/sustech/cs307/aggregation/SumFunction.java
@@ -1,42 +1,55 @@
 package edu.sustech.cs307.aggregation;
 
 import edu.sustech.cs307.exception.DBException;
+import edu.sustech.cs307.meta.TabCol;
+import edu.sustech.cs307.tuple.TableTuple;
 import edu.sustech.cs307.tuple.Tuple;
 import edu.sustech.cs307.value.Value;
 import edu.sustech.cs307.value.ValueType;
 
 public class SumFunction implements AggregateFunction {
     private final String columnName;
+    private final TabCol parameter;
     private Value sum;
+    double sum_value;
 
-    public SumFunction(String columnName) {
+    public SumFunction(String columnName, TabCol parameter) {
         this.columnName = columnName;
-        this.sum = new Value(Long.valueOf(0)); // 初始化为零值
+        this.parameter = parameter;
+        sum_value = 0.0;
+        this.sum = new Value(Double.valueOf(sum_value)); // 初始化为零值
+    }
+
+
+    @Override
+    public void reset() {
+        sum_value = 0.0F;
+        this.sum = new Value(Double.valueOf(sum_value));
     }
 
     @Override
-    public AggregateFunction newInstance() {
-        return new SumFunction(columnName);
+    public void accumulate(Value v) {
+        double d = Double.valueOf(v.toString());
+        sum_value += d;
     }
 
     @Override
-    public void accumulate(Tuple tuple) throws DBException {
-//        Value value = tuple.getColumnValue(columnName);
-//        sum = sum.add(value);
+    public Value result() {
+        return new Value(Double.valueOf(sum_value));
     }
 
     @Override
-    public Object getResult() {
-        return sum;
+    public ValueType outputType() {
+        return ValueType.FLOAT;
     }
 
     @Override
-    public ValueType getType() {
-        return sum.getType();
-    }
-
-    @Override
-    public String getAlias() {
+    public String alias() {
         return "SUM(" + columnName + ")";
+    }
+
+    @Override
+    public TabCol getTabCol() {
+        return parameter;
     }
 }
